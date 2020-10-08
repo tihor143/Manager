cac35ab23df903ebdae80fbf925511873a091900
tihor143 <71205900+tihor143@users.noreply.github.com>
2020-10-08 16:17:03 +0530

    Update __main__.py

--- Manager/__main__.py
+++ Manager/__main__.py
@@ -24,7 +24,7 @@ Hello {}, my name is {}!
 
 You know how hard it is sometimes to manage group so here is the solution for you.
 
-My owner is [Tihor 🇮🇳](t.me/tihor2)
+My owner is [Harsha 🇮🇳](t.me/harsha_chaudhary_104)
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 """
